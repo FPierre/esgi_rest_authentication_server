045010227f4fdb3963efa00e0858dc9008b69b76
FPierre <pflauder@gmail.com>
2016-06-05 12:23:51 +0200

    Ecriture de la documentation

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # esgi_rest_authentication_server
 
+> Serveur d'authentification d'un utilisateur.
+
+## Requis
+
+* Node.js
+
 Lancer le serveur
 
 ```bash
--- server.js
+++ server.js
@@ -10,22 +10,24 @@ var User   = require('./app/models/user')
 
 // Configuration
 
+// Serveur sur le port 8080
 var port = process.env.PORT || 8080
 
 mongoose.connect(config.database)
 app.set('superSecret', config.secret)
 
-// Use body parser so we can get info from POST and/or URL parameters
+// Parser de requetes HTTP
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// Use morgan to log requests to the console
+// Log des actions du serveur
 app.use(morgan('dev'))
 
 // Routes
 
+// Route de remplissage de la DB
 app.get('/setup', function (req, res) {
-  // Create a sample user
+  // Création d'un Utilisateur
   var myself = new User({
     login: 'pflauder@gmail.com',
     password: 'root'
@@ -44,7 +46,7 @@ app.get('/setup', function (req, res) {
 
 var apiRoutes = express.Router()
 
-// Authentication (no middleware necessary since this isnt authenticated)
+// Routes d'authentification
 apiRoutes.post('/authenticate', function (req, res) {
   User.findOne({
     login: req.body.login
@@ -54,14 +56,13 @@ apiRoutes.post('/authenticate', function (req, res) {
     }
 
     if (user) {
-      // Check if password matches
       if (user.password != req.body.password) {
         res.json({ success: false, message: 'Authentication failed. Wrong password.' })
       }
       else {
-        // Create a token
+        // Création d'un token
         var token = jwt.sign(user, app.get('superSecret'), {
-          expiresIn: 86400 // expires in 24 hours
+          expiresIn: 86400 // 24h
         })
 
         res.json({
@@ -77,28 +78,22 @@ apiRoutes.post('/authenticate', function (req, res) {
   })
 })
 
-// Route middleware to authenticate and check token
+// Gestion de l'authentification
 apiRoutes.use(function (req, res, next) {
-  // check header or url parameters or post parameters for token
   var token = req.body.token || req.param('token') || req.headers['x-access-token']
 
-  // Decode token
   if (token) {
-    // Verifies secret and checks exp
     jwt.verify(token, app.get('superSecret'), function (err, decoded) {
       if (err) {
         return res.json({ success: false, message: 'Failed to authenticate token.' })
       }
       else {
-        // If everything is good, save to request for use in other routes
         req.decoded = decoded
         next()
       }
     })
   }
   else {
-    // If there is no token
-    // return an error
     return res.status(403).send({
       success: false,
       message: 'No token provided.'
@@ -106,24 +101,13 @@ apiRoutes.use(function (req, res, next) {
   }
 })
 
-// Authenticated routes
 apiRoutes.get('/', function (req, res) {
   res.json({ message: 'Welcome to the coolest API on earth!' })
 })
 
-apiRoutes.get('/users', function (req, res) {
-  User.find({}, function (err, users) {
-    res.json(users)
-  })
-})
-
-apiRoutes.get('/check', function (req, res) {
-  res.json(req.decoded)
-})
-
+// Routes montées sur /api
 app.use('/api', apiRoutes)
 
-// Get an instance of the router for api routes
 var apiRoutes = express.Router()
 
 app.listen(port)
